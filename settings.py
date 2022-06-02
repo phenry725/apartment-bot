20f8675f32845cdf4fb5081b5739edd8f5cd9824
mikeh777 <mhenry@alumni.scu.edu>
2022-06-02 17:27:51 -0400

    Change messenger service from slack to twilio

--- settings.py
+++ settings.py
@@ -1,101 +1,51 @@
 import os
 
-##Slack variables
-SLACK_TOKEN = os.getenv('SLACK_TOKEN', "")
-if SLACK_TOKEN == "":
-    print("No slack token configured, please add an EXPORT SLACK_TOKEN = to your bash_profile")
-    exit()
-SLACK_CHANNEL = "#housing"
-SLACK_BOT_NAME = "housingBot"
+##Twilio variables
+ACCOUNT_SID = os.environ["ACCOUNT_SID"]
+AUTH_TOKEN = os.environ["AUTH_TOKEN"]
+MS_SID = os.environ["MS_SID"]
+TARGET_PHONE_NUMBER = os.environ["PHONE_NUMBER"]
 
 ##Craigslist variables
 CL_SITE = 'sfbay'
 CL_AREA = 'sfc'
 CL_CATEGORY = 'apa'
-MAX_NUM_BEDROOMS = 3
+MAX_NUM_BEDROOMS = 2
 MIN_NUM_BEDROOMS = 1
 POSTED_TODAY = True
-MAX_PRICE = 4100
-MIN_PRICE = 1000
+MAX_PRICE = 4000
+MIN_PRICE = 1700
 LAUNDRY_OPTIONS = ['w/d hookups', 'w/d in unit', 'laundry in bldg','laundry on site']
-PARKING_OPTIONS = ['carport', 'attached garage', 'detached garage', 'off-street parking', 'street parking']
+#PARKING_OPTIONS = ['carport', 'attached garage', 'detached garage', 'off-street parking', 'street parking']
 HOUSING_TYPE = ['apartment', 'condo', 'flat', 'house', 'loft']
-
 
 ##Housing variables
 #define areas that we want to live in
 AREAS = {
-    "north-city": [
-        [-122.424637,37.788507],
-        [-122.399478,37.804217]
+    "russian-hill": [
+        [-122.424929,37.795946],
+        [-122.410338,37.805179]
     ],
-    "dem-heights": [
-        [-122.44721,37.786682],
-        [-122.422328,37.800982]
+    "nob-hill":
+    [
+        [-122.421748, 37.788999],
+        [-122.408916, 37.799477]
     ],
-    "jtown-lph-wa-hayesvalley": [
-        [-122.444552,37.770813],
-        [-122.419307,37.788262]
+    "cow-hollow":
+    [
+        [-122.442464, 37.793427],
+        [-122.426413, 37.800617]
     ],
-    "panhandle": [
-        [-122.453367,37.760502],
-        [-122.428843,37.774267]
-    ],
-    "lower-city potrero hill": [
-        [-122.412103,37.749715],
-        [-122.388665,37.767276]
-    ],
-    "presidio": [
-        [-122.480929,37.772603],
-        [-122.445637,37.803769]
+    "marina":
+    [
+        [-122.447298, 37.799412],
+        [-122.424853, 37.806601]
     ]
-
 }
-
-# {
-#     "russian-hill": [
-#         [-122.424929,37.795946],
-#         [-122.414543,37.805179]
-#     ],
-#     "pac-heights": [
-#         [-122.440493,37.787825],
-#         [-122.422799,37.804067]
-#     ],
-#     "lower-pac-heights": [
-#         [-122.443505,37.78306],
-#         [-122.423056,37.791819]
-#     ],
-#     "hayes-duboce-mission": [
-#         [-122.4391754823,37.7571045379],
-#         [-122.4167884802,37.7800723458]
-#     ],
-#     "the-emma-zone": [
-#         [-122.472109,37.756633],
-#         [-122.427535,37.778873]
-#     ]
-# }
 #fall back neighborhoods to try and to find in the listing string
 NEIGHBORHOODS = ["russian hill",
-                 "soma",
-                 "pac heights",
-                 "pacific heights",
-                 "lower pac hts",
                  "nob hill",
-                 "hayes valley",
-                 "duboce triangle",
-                 "mission",
-                 "nopa",
-                 "panhandle",
-                 "lower haight",
                  "marina",
-                 "cow hollow",
-                 "potrero hill",
-                 "alamo square",
-                 "inner sunset",
-                 "cole valley",
-                 "noe valley",
-                 "west portal",
-                 "forest hill"
-                 ]
+                 "cow hollow"]
 
 SLEEP_TIMER = 20 * 60
